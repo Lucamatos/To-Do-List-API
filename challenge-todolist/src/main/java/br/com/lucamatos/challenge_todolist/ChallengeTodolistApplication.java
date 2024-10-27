9797f8ed08191214cf5ea293aefe73169f0be32f
Luca Matos <107355875+Lucamatos@users.noreply.github.com>
2024-10-26 21:36:33 -0300

    To Do List API first commit
    First commit of my to do list REST API curd project utilizing Java Spring Boot

---
+++ challenge-todolist/src/main/java/br/com/lucamatos/challenge_todolist/ChallengeTodolistApplication.java
@@ -0,0 +1,14 @@
+package br.com.lucamatos.challenge_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeTodolistApplication.class, args);
+        System.out.println("Hello World");
+    }
+
+}
