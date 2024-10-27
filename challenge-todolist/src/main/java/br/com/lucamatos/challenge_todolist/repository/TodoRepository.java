666fd53a2ba0cd0187e40ad0ef59a0b5303254a3
Luca Matos <107355875+Lucamatos@users.noreply.github.com>
2024-10-26 23:25:35 -0300

    ToDo Service and Controller
    ToDo Service and Controller main functions created

--- challenge-todolist/src/main/java/br/com/lucamatos/challenge_todolist/repository/TodoRepository.java
+++ challenge-todolist/src/main/java/br/com/lucamatos/challenge_todolist/repository/TodoRepository.java
@@ -1,4 +1,7 @@
 package br.com.lucamatos.challenge_todolist.repository;
 
-public class TodoRepository {
+import br.com.lucamatos.challenge_todolist.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
 }
